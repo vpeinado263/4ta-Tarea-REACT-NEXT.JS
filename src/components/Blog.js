dac808a0fe4dca1143ad5a4dea398c8059e72957
Victor Peinado <vpeinado263@gmail.com>
2024-10-25 16:44:47 -0300

    Agregar datos a cada seccion

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,6 +3,15 @@ import { useState, useEffect } from "react";
 const Blog = () => {
 
   const [recurso, setRecurso] = useState("posteo");
+  const [contenido, setContenido] = useState("")
+
+  //colocamos los datos de cada seccion
+
+  const datos = {
+    posteos: "Bienvenidos al blog sobre la creación del reloj. Aquí exploraremos los pasos para diseñar un reloj único.",
+    usuarios: "Autores destacados: Abraham-Louis Breguet,  Christiaan Huygens, Daniel Quare, John Harwood, Franz Ketterer",
+    comentarios: "comentarios: ¡Qué interesante! Me encantaría aprender más sobre la historia de los relojes.",
+  }
 
   useEffect(() => {}, [recurso])
 
