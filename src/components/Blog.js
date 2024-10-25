6e3caf393fb0d97122432d84b5db594758bfc906
Victor Peinado <vpeinado263@gmail.com>
2024-10-25 14:51:44 -0300

    Añadir configuración a Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
 const Blog = () => {
+
+  const [recurso, setRecurso] = useState("posteo");
+
+  useEffect(() => {}, [recurso])
+
   return (
     <>
-      
+      <div>
+        <button onClick={() => setRecurso("posteos")}>Posteos</button>
+        <button onClick={() => setRecurso("usuarios")}>Usuario</button>
+        <button onClick={() => setRecurso("comentarios")}>Comentarios</button>
+      </div>
+      <h2>{recurso}</h2>
     </>
   )
 }
