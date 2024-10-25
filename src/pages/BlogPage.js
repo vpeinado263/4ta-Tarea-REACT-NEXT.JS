5167e3045c8d614c53f45347d4d4da730734831d
Victor Peinado <vpeinado263@gmail.com>
2024-10-25 18:56:12 -0300

    Crear page BlogPage.js

---
+++ src/pages/BlogPage.js
@@ -0,0 +1,11 @@
+
+import BlogTemplate from '@/components/template/BlogTemplate'
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <BlogTemplate/>
+  )
+}
+
+export default BlogPage;
