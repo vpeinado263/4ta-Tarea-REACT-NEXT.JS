e0dda057a4ca8bafd1f22377a21c90c00744afb6
Victor Peinado <vpeinado263@gmail.com>
2024-10-25 18:07:10 -0300

    Crear compnente ImageBlog y su hoja de estilos

---
+++ src/components/molecules/ImagenBlog.js
@@ -0,0 +1,11 @@
+import styles from "./imageBlog.module.css";
+
+const ImagenBlog = ({}) => {
+  return (
+    <div className={styles.imageContainer}>
+      <img src="" alt="" className={styles.image}/>
+    </div>
+  )
+}
+
+export default ImagenBlog;
